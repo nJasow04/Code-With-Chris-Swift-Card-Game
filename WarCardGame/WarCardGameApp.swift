51598992bf047b8ba058ebdab59f2827edfe33ae
Jason Wan <njasow@g.ucla.edu>
2024-04-17 21:27:03 -0400

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by jason wan on 2024-04-17.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
